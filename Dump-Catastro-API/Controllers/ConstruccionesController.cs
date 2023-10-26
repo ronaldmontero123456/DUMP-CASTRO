99956dfd20bf6ab22e3720d6e952216fcbefb69e
r.montero <r.montero@LAPTOP-GR01EI04>
2023-10-25 21:57:19 -0400

    ...

--- Dump-Catastro-API/Controllers/ConstruccionesController.cs
+++ Dump-Catastro-API/Controllers/ConstruccionesController.cs
@@ -1,6 +1,10 @@
-﻿using Dump_Catastro_CORE.Interfaces;
+﻿using Dump_Catastro_CORE.Entities;
+using Dump_Catastro_CORE.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NetTopologySuite.IO;
+using Newtonsoft.Json;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Dump_Catastro_API.Controllers
 {
@@ -18,14 +22,21 @@ namespace Dump_Catastro_API.Controllers
         [HttpGet(Name = nameof(GetBuilding))]
         public IActionResult GetBuilding()
         {
-            var building = _unitOfWork.VistaConstruccioneRepository.GetAll().Take(50).ToList();
+            var geoJsonWriter = new GeoJsonWriter();
+
+            var construcciones = _unitOfWork.VistaConstruccioneRepository.GetAll().Select(vista => new
+            {
+                Properties = JsonConvert.DeserializeObject<VistaConstruccione>(vista.Properties),
+                vista.Type,
+                Geometry = JsonConvert.DeserializeObject<GeoJsonPolygon>(geoJsonWriter.Write(vista.Geometry))
+            });
 
             var response = new
             {
                 error = false,
                 found = true,
                 message = "",
-                data = building
+                data = construcciones
             };
 
             return Ok(response);
